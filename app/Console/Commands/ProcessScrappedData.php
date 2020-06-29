bff8998fa3f4229271dba9f29d16913ffc146ab5
illia <illia.kyzlaitis.cv@gmail.com>
2020-06-30 01:58:03 +0300

    second crop type

--- app/Console/Commands/ProcessScrappedData.php
+++ app/Console/Commands/ProcessScrappedData.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\MarketData;
 use App\Models\ScrappedDataRecord;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
 
 class ProcessScrappedData extends Command
 {
@@ -43,58 +44,62 @@ class ProcessScrappedData extends Command
 
         foreach ($data as $record) {
             $decodedScrappedData = json_decode($record->scrapped_data);
+            $hash                = md5($record->scrapped_data);
+            $marketData          = MarketData::where('hash', $hash)->first();
+            if (!$marketData) {
+                (MarketData::create(
+                    [
+                        'culture_en'               => $decodedScrappedData->cultureEn,
+                        'delivery_due_data_en'     => $decodedScrappedData->deliveryDueDataEn,
+                        'delivery_terms_en'        => $decodedScrappedData->deliveryTermsEn,
+                        'description_en'           => $decodedScrappedData->descriptionEn,
+                        'location_en'              => $decodedScrappedData->locationEn,
+                        'month_of_delivery_en'     => $decodedScrappedData->monthOfDeliveryEn,
+                        'offer_type_en'            => $decodedScrappedData->offerTypeEn,
+                        'price_en'                 => $decodedScrappedData->priceEn,
+                        'processing_company_en'    => $decodedScrappedData->processingCompanyEn,
+                        'valid_until_en'           => $decodedScrappedData->validUntilEn,
+                        'vat_en'                   => $decodedScrappedData->vatEn,
+                        'volume_en'                => $decodedScrappedData->volumeEn,
+                        'company_name_en'          => $decodedScrappedData->companyNameEn,
+                        'company_contact_en'       => $decodedScrappedData->companyContactEn,
+                        'company_telephone_en'     => $decodedScrappedData->companyTelephoneEn,
+                        'company_website_en'       => $decodedScrappedData->companyWebsiteEn,
+                        'company_registered_no_en' => $decodedScrappedData->companyRegisteredNoEn,
+                        'company_director_en'      => $decodedScrappedData->companyDirectorEn,
+                        'company_owner_en'         => $decodedScrappedData->companyOwnerEn,
+                        'company_type_en'          => $decodedScrappedData->companyTypeEn,
+                        'company_address_en'       => $decodedScrappedData->companyAddressEn,
+                        'published_at_en'          => $decodedScrappedData->publishedEn,
 
+                        'culture_uk'               => $decodedScrappedData->cultureUk,
+                        'delivery_due_data_uk'     => $decodedScrappedData->deliveryDueDataUk,
+                        'delivery_terms_uk'        => $decodedScrappedData->deliveryTermsUk,
+                        'description_uk'           => $decodedScrappedData->descriptionUk,
+                        'location_uk'              => $decodedScrappedData->locationUk,
+                        'month_of_delivery_uk'     => $decodedScrappedData->monthOfDeliveryUk,
+                        'offer_type_uk'            => $decodedScrappedData->offerTypeUk,
+                        'price_uk'                 => $decodedScrappedData->priceUk,
+                        'processing_company_uk'    => $decodedScrappedData->processingCompanyUk,
+                        'valid_until_uk'           => $decodedScrappedData->validUntilUk,
+                        'vat_uk'                   => $decodedScrappedData->vatUk,
+                        'volume_uk'                => $decodedScrappedData->volumeUk,
+                        'company_name_uk'          => $decodedScrappedData->companyNameUk,
+                        'company_contact_uk'       => $decodedScrappedData->companyContactUk,
+                        'company_telephone_uk'     => $decodedScrappedData->companyTelephoneUk,
+                        'company_website_uk'       => $decodedScrappedData->companyWebsiteUk,
+                        'company_registered_no_uk' => $decodedScrappedData->companyRegisteredNoUk,
+                        'company_director_uk'      => $decodedScrappedData->companyDirectorUk,
+                        'company_owner_uk'         => $decodedScrappedData->companyOwnerUk,
+                        'company_type_uk'          => $decodedScrappedData->companyTypeUk,
+                        'company_address_uk'       => $decodedScrappedData->companyAddressUk,
+                        'published_at_uk'          => $decodedScrappedData->publishedUk,
+                        'hash'                     => $hash,
 
-            (MarketData::create(
-                [
-                    'culture_en'               => $decodedScrappedData->cultureEn,
-                    'delivery_due_data_en'     => $decodedScrappedData->deliveryDueDataEn,
-                    'delivery_terms_en'        => $decodedScrappedData->deliveryTermsEn,
-                    'description_en'           => $decodedScrappedData->descriptionEn,
-                    'location_en'              => $decodedScrappedData->locationEn,
-                    'month_of_delivery_en'     => $decodedScrappedData->monthOfDeliveryEn,
-                    'offer_type_en'            => $decodedScrappedData->offerTypeEn,
-                    'price_en'                 => $decodedScrappedData->priceEn,
-                    'processing_company_en'    => $decodedScrappedData->processingCompanyEn,
-                    'valid_until_en'           => $decodedScrappedData->validUntilEn,
-                    'vat_en'                   => $decodedScrappedData->vatEn,
-                    'volume_en'                => $decodedScrappedData->volumeEn,
-                    'company_name_en'          => $decodedScrappedData->companyNameEn,
-                    'company_contact_en'       => $decodedScrappedData->companyContactEn,
-                    'company_telephone_en'     => $decodedScrappedData->companyTelephoneEn,
-                    'company_website_en'       => $decodedScrappedData->companyWebsiteEn,
-                    'company_registered_no_en' => $decodedScrappedData->companyRegisteredNoEn,
-                    'company_director_en'      => $decodedScrappedData->companyDirectorEn,
-                    'company_owner_en'         => $decodedScrappedData->companyOwnerEn,
-                    'company_type_en'          => $decodedScrappedData->companyTypeEn,
-                    'company_address_en'       => $decodedScrappedData->companyAddressEn,
-                    'published_at_en'           => $decodedScrappedData->publishedEn,
-
-                    'culture_uk'               => $decodedScrappedData->cultureUk,
-                    'delivery_due_data_uk'     => $decodedScrappedData->deliveryDueDataUk,
-                    'delivery_terms_uk'        => $decodedScrappedData->deliveryTermsUk,
-                    'description_uk'           => $decodedScrappedData->descriptionUk,
-                    'location_uk'              => $decodedScrappedData->locationUk,
-                    'month_of_delivery_uk'     => $decodedScrappedData->monthOfDeliveryUk,
-                    'offer_type_uk'            => $decodedScrappedData->offerTypeUk,
-                    'price_uk'                 => $decodedScrappedData->priceUk,
-                    'processing_company_uk'    => $decodedScrappedData->processingCompanyUk,
-                    'valid_until_uk'           => $decodedScrappedData->validUntilUk,
-                    'vat_uk'                   => $decodedScrappedData->vatUk,
-                    'volume_uk'                => $decodedScrappedData->volumeUk,
-                    'company_name_uk'          => $decodedScrappedData->companyNameUk,
-                    'company_contact_uk'       => $decodedScrappedData->companyContactUk,
-                    'company_telephone_uk'     => $decodedScrappedData->companyTelephoneUk,
-                    'company_website_uk'       => $decodedScrappedData->companyWebsiteUk,
-                    'company_registered_no_uk' => $decodedScrappedData->companyRegisteredNoUk,
-                    'company_director_uk'      => $decodedScrappedData->companyDirectorUk,
-                    'company_owner_uk'         => $decodedScrappedData->companyOwnerUk,
-                    'company_type_uk'          => $decodedScrappedData->companyTypeUk,
-                    'company_address_uk'       => $decodedScrappedData->companyAddressUk,
-                    'published_at_uk'           => $decodedScrappedData->publishedUk,
-
-                ]
-            ))->save();
+                    ]
+                ))->save();
+                $record->delete();
+            }
         }
     }
 }
--- database/migrations/2020_06_25_224319_create_market_data_ua.php
+++ database/migrations/2020_06_25_224319_create_market_data_ua.php
@@ -17,9 +17,9 @@ class CreateMarketDataUa extends Migration
             'market_data',
             function (Blueprint $table) {
                 $table->id();
-                $table->string('culture_en', 512);
+                $table->string('culture_en');
                 $table->string('delivery_due_data_en');
-                $table->string('delivery_terms_en', 128);
+                $table->string('delivery_terms_en');
                 $table->text('description_en');
                 $table->string('location_en');
                 $table->string('month_of_delivery_en');
@@ -29,21 +29,21 @@ class CreateMarketDataUa extends Migration
                 $table->string('valid_until_en');
                 $table->string('vat_en');
                 $table->string('volume_en');
-                $table->string('company_address_en', 512);
+                $table->string('company_address_en');
 
-                $table->string('company_name_en', 512);
-                $table->string('company_contact_en', 512);
-                $table->string('company_telephone_en', 512);
+                $table->string('company_name_en');
+                $table->string('company_contact_en');
+                $table->string('company_telephone_en');
                 $table->string('company_website_en');
-                $table->string('company_registered_no_en', 512);
-                $table->string('company_director_en', 512);
-                $table->string('company_owner_en', 512);
-                $table->string('company_type_en', 512);
+                $table->string('company_registered_no_en');
+                $table->string('company_director_en');
+                $table->string('company_owner_en');
+                $table->string('company_type_en');
 
 
-                $table->string('culture_uk', 512);
+                $table->string('culture_uk');
                 $table->string('delivery_due_data_uk');
-                $table->string('delivery_terms_uk', 512);
+                $table->string('delivery_terms_uk');
                 $table->text('description_uk');
                 $table->string('location_uk');
                 $table->string('month_of_delivery_uk');
@@ -54,17 +54,18 @@ class CreateMarketDataUa extends Migration
                 $table->string('vat_uk');
                 $table->string('volume_uk');
 
-                $table->string('company_name_uk', 512);
-                $table->string('company_contact_uk', 512);
-                $table->string('company_telephone_uk', 512);
-                $table->string('company_website_uk', 512);
-                $table->string('company_registered_no_uk', 512);
-                $table->string('company_director_uk', 512);
-                $table->string('company_owner_uk', 512);
-                $table->string('company_type_uk', 512);
-                $table->string('company_address_uk', 512);
-                $table->string('published_at_uk', 512);
-                $table->string('published_at_en', 512);
+                $table->string('company_name_uk');
+                $table->string('company_contact_uk');
+                $table->string('company_telephone_uk');
+                $table->string('company_website_uk');
+                $table->string('company_registered_no_uk');
+                $table->string('company_director_uk');
+                $table->string('company_owner_uk');
+                $table->string('company_type_uk');
+                $table->string('company_address_uk');
+                $table->string('published_at_uk');
+                $table->string('published_at_en');
+                $table->string('hash')->default('');
             }
         );
     }
@@ -76,34 +77,8 @@ class CreateMarketDataUa extends Migration
      */
     public function down()
     {
-        Schema::create(
-            'market_data',
-            function (Blueprint $table) {
-                $table->dropColumn('culture_uk');
-                $table->dropColumn('delivery_due_data_uk');
-                $table->dropColumn('delivery_terms_uk');
-                $table->dropColumn('description_uk');
-                $table->dropColumn('location_uk');
-                $table->dropColumn('month_of_delivery_uk');
-                $table->dropColumn('offer_type_uk');
-                $table->dropColumn('price_uk');
-                $table->dropColumn('processing_company_uk');
-                $table->dropColumn('valid_until_uk');
-                $table->dropColumn('vat_uk');
-                $table->dropColumn('volume_uk');
+        Schema::drop(
+            'market_data');
 
-                $table->dropColumn('company_name_uk');
-                $table->dropColumn('company_contact_uk');
-                $table->dropColumn('company_telephone_uk');
-                $table->dropColumn('company_website_uk');
-                $table->dropColumn('company_registered_no_uk');
-                $table->dropColumn('company_director_uk');
-                $table->dropColumn('company_owner_uk');
-                $table->dropColumn('company_type_uk');
-                $table->dropColumn('company_address_uk');
-                $table->dropColumn('published_at_uk');
-                $table->dropColumn('published_at_en');
-            }
-        );
     }
 }
