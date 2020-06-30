33c7b5a3b63bab835b8f2779a04bb84b58b0dca9
illia <illia.kyzlaitis.cv@gmail.com>
2020-07-01 01:18:49 +0300

    second crop type

--- scrapper/sequelize-config.js
+++ scrapper/sequelize-config.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 // DB_DATABASE=scrapper
 // DB_USERNAME=root
 // DB_PASSWORD=my-secret-pw
-const sequelize = new Sequelize('scrapper', 'root', 'my-secret-pw', {
+const sequelize = new Sequelize('oilmarket', 'oilmarket', '[eqgbplf;jgf', {
     host: 'localhost',
     port: '33066',
     dialect: 'mysql',
