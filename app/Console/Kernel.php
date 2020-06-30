51d47ca6264478123ccf4e203ff007d3aab53599
illia <illia.kyzlaitis.cv@gmail.com>
2020-07-01 01:49:48 +0300

    second crop type

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->exec('node /var/www/podiliyagold.com/scrapper/index.js')->dailyAt('02:00');
+        $schedule->exec('node /var/www/podiliyagold.com/scrapper/index.js')->dailyAt('01:52');
 //        $schedule->exec('ping 8.8.8.8')->everyMinute();
     }
 
