36805691a57d963c3d38a98dd24081705d238017
illia <illia.kyzlaitis.cv@gmail.com>
2020-09-24 23:25:14 +0300

    Glue. Match sticks. Paper. Build script!

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -18,6 +19,17 @@ class ContactController extends Controller
                 'message' => $request->message ?? '',
             ]
         );
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom("noreply@podiliyagold.com", "Example User");
+        $email->setSubject("Test illia");
+        $email->addTo("illia.kyzlaitis.cv@gmail.com", "Example User");
+        $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+        $email->addContent(
+            "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+        );
+        $sendgrid = new \SendGrid(getenv(env('SENDGRID')));
+
+        $response = $sendgrid->send($email);
 
         return response()->json(['success' => true], 201);
     }
